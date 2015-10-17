6ce6d0c9cbf19d899d1aa7933f0f964594408f66
Sergey Rezvanov <sergey@rezvanov.info>
2015-10-17 22:23:29 +0500

    Add Rails 4.2 test app

---
+++ spec/apps/rails_4.2/app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  respond_to :html
+
+  def index
+    respond_with({})
+  end
+end
