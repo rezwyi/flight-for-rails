4b6f8fe80ac329f4a7fa7d4a0803195bcdae0b90
Sergey Rezvanov <sergey@rezvanov.info>
2015-10-17 22:23:20 +0500

    Add Rails 4.1 test app

---
+++ spec/apps/rails_4.1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4::Application.config.secret_key_base = '63e5c937bbf5e2bd70c799af9e742057cbca7363ce428a3fd1208d50a88fa614c62d550d0fa34212f922a9d36a18ef3d13b14433f47bded40d8011105c84e78e'
